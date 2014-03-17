5a0814985cb0b72e900bf07d165c283e9c99d251
80LevelElf <salahutd@gmail.com>
2014-03-17 20:13:24 +0400

    First adding of code.
    Signed-off-by: 80LevelElf <salahutd@gmail.com>

---
+++ Scripts/Mather/Math.js
@@ -0,0 +1,17 @@
+﻿var math = mathjs();
+
+function Calculate(expression) {
+    var result =
+    {
+        answer: null,
+        error: null
+    };
+
+    try {
+        result.answer = math.format(math.eval(expression));
+    } catch (e) {
+        result.error = e.message;
+    }
+
+    return result;
+}
